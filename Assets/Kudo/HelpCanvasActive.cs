af06171ac5cd07a2e6d350b518162671d12b5a33
Otsu01 <kudo.nonoka02@gmail.com>
2023-02-06 19:41:31 +0900

    [update]Titleシーン移動の時スコアを0にした[add]Resultシーンのスコアを表示できるようにした

---
+++ Assets/Kudo/HelpCanvasActive.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpCanvasActive : MonoBehaviour
+{
+    [SerializeField] GameObject _helpCanvas;
+    bool _isCanvas = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _helpCanvas.SetActive(_isCanvas);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HelpCanvasActiveControl()
+    {
+        _isCanvas = !_isCanvas;
+        _helpCanvas.SetActive(_isCanvas);
+    }
+}
